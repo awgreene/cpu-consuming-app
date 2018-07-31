872d2034cf14da951357084f3be6f5d48dcd2e6e
agreene <agreene@redhat.com>
2018-07-31 09:19:05 -0400

    Add new endpoint for bottleneck simulation

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
   "github.com/gorilla/mux"
   "encoding/json"
   "time"
+  "sync"
 )
+
+// Used for increasingly slower lag
 var lag time.Duration
 
+// Used to simulate bottleneck
+var mutex = &sync.Mutex{}
+
+// Simulates high cpu utilization
 func hitCpu(w http.ResponseWriter, r *http.Request) {
   wait := 9999999999
   for i:=0; i<wait; i++ {
@@ -17,7 +24,7 @@ func hitCpu(w http.ResponseWriter, r *http.Request) {
   json.NewEncoder(w).Encode("{result: 200}")
 }
 
-// Endpoint that returns longer and longer wait times as you visit it.
+// Simulates endpoint that becomes increasingly unresponsive as visited
 func simulateLag(w http.ResponseWriter, r *http.Request) {
   wait := lag
   lag++
@@ -30,10 +37,26 @@ func resetLag(w http.ResponseWriter, r *http.Request) {
   lag = 0
 }
 
+// Simulate bottleneck with locked resource
+func lock(w http.ResponseWriter, r *http.Request) {
+  mutex.Lock()
+  time.Sleep(2 * time.Second)
+  mutex.Unlock()
+  json.NewEncoder(w).Encode("{result: 200}")
+}
+
+// Simulates 1s wait
+func wait(w http.ResponseWriter, r *http.Request) {
+  time.Sleep(1 * time.Second)
+  json.NewEncoder(w).Encode("{result: 200}")
+}
+
 func main() {
   router := mux.NewRouter()
+  router.HandleFunc("/cpu", hitCpu).Methods("GET")
   router.HandleFunc("/lag", simulateLag).Methods("GET")
   router.HandleFunc("/lag/reset", resetLag).Methods("GET")
-  router.HandleFunc("/cpu", hitCpu).Methods("GET")
+  router.HandleFunc("/lock", lock).Methods("GET")
+  router.HandleFunc("/1s-wait", wait).Methods("GET")
   log.Fatal(http.ListenAndServe(":8000", router))
 }
